464f87d0e12d627269693e3339c72e49ffdf06b8
FroskyLT <kinglync1@gmail.com>
2020-12-08 23:05:34 +0200

    Finish assembly code

--- assembler/assembler.cpp
+++ assembler/assembler.cpp
@@ -1,20 +1,47 @@
-// assembler.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+    if (argc < 2) { 
+        std::cout << "Missing parameter";
+        return 0;
+    }
+    if (argc > 2) {
+        std::cout << "To many arguments";
+        return 0;
+    }
+
+    char* c = argv[1];
+    int result;
+
+    __asm {
+        push eax
+        push ebx
+        push ecx
+
+        mov ebx, c
+        mov ecx, 0
+
+        forloop:
+            movzx al, byte ptr[ebx]
+            cmp al, 0
+            jz finish
+            sub eax, 48
+            imul ecx, 10
+            add ecx, eax
+            inc ebx
+            jnz	forloop
+
+        finish:
+            mov result, ecx
+
+        pop eax
+        pop ebx
+        pop ecx
+    }
+
+    std::cout << result;
+
+        
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
