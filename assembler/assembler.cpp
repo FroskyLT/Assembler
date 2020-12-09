0e9106817e1dfebcd78657ba08548572899166a8
FroskyLT <kinglync1@gmail.com>
2020-12-09 12:05:00 +0200

    add test loop

--- assembler/assembler.cpp
+++ assembler/assembler.cpp
@@ -18,26 +18,48 @@ int main(int argc, char** argv)
         push eax
         push ebx
         push ecx
+        push edx
 
         mov ebx, c
-        mov ecx, 0
+        mov ecx, c
+        mov edx, 0
 
+        testloop:
+            movzx al, byte ptr[ecx]
+            cmp al, 0
+            jz startforloop
+
+            cmp edx, 10
+            jz gofinish
+
+            inc ecx
+            inc edx
+            jnz	testloop
+
+
+        startforloop:
+            mov edx, 0
         forloop:
             movzx al, byte ptr[ebx]
             cmp al, 0
             jz finish
+
             sub eax, 48
-            imul ecx, 10
-            add ecx, eax
+            imul edx, 10
+            add edx, eax
             inc ebx
             jnz	forloop
 
+
+        gofinish:
+            mov edx, 0
         finish:
-            mov result, ecx
+            mov result, edx
 
         pop eax
         pop ebx
         pop ecx
+        pop edx
     }
 
     std::cout << result;
